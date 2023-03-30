443c1c0f6565d882854b71e8703afff66c420ab9
Isora Dolage <69623047+idolage@users.noreply.github.com>
2023-03-30 14:36:57 +0530

    Updatedd web  application

--- Website/server/util.py
+++ Website/server/util.py
@@ -12,7 +12,7 @@ from firebase_admin import credentials, db
 import pandas as pd
 
 cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {"databaseURL": "DB_URL"})
+firebase_admin.initialize_app(cred, {"databaseURL": 'DB_URL'})
 transactions_ref = db.reference('transactions')
 
 __data_columns = None
